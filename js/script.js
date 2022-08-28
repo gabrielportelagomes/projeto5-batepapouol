b6fa4c2be773fb9a0d5068dcc470356ebd724ac4
Gabriel <gabrielgomesportela@gmail.com>
2022-08-28 12:17:42 -0300

    layout improved and send message feature implemented

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 let userLog = [];
 let userStatus = [];
 let user = '';
-let message = [];
+let message = '';
+let postedMessage = [];
+let postMessage = [];
 
 function menu() {
     const activateMenu = document.querySelector('aside');
@@ -13,12 +15,29 @@ function hideMenu() {
     hideMenu.classList.add('hide');
 }
 
+function acivateAnimation() {
+    const hideLog = document.querySelector('.user');
+    hideLog.classList.add('hide');
+
+    const activateLoading = document.querySelector('.loading');
+    activateLoading.classList.remove('hide');
+}
+
+function hideAnimation() {
+    const hideLog = document.querySelector('.user');
+    hideLog.classList.remove('hide');
+
+    const activateLoading = document.querySelector('.loading');
+    activateLoading.classList.add('hide');
+}
+
 function hideEntryScreen() {
     const hideMenu = document.querySelector('.entry-screen');
     hideMenu.classList.add('hide');
 }
 
 function nameId() {
+    acivateAnimation()
     user = document.querySelector('#name').value;
 
     userLog = { name: user };
@@ -30,14 +49,11 @@ function nameId() {
 
 function invalidName() {
     alert("Nome de usuário inválido ou já cadastrado!\nDigite outro nome de usuário.");
+    hideAnimation();
 }
 
 function renderError() {
     console.log("Ocorreu um erro!");
-}
-
-function renderItWorked() {
-    console.log("Tudo certo!");
 }
 
 function getMessage() {
@@ -45,40 +61,69 @@ function getMessage() {
     setInterval(statusLog, 5000);
     const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
     promise.then(renderMessage);
-    promise.catch(renderError);
+    promise.catch(console.log("Erro ao renderizar menssagens"));
 }
 
 function renderMessage(answer) {
-    console.log(answer);
-    message = answer.data;
+    /* console.log(answer); */
+    postedMessage = answer.data;
 
 
     let listMessage = document.querySelector(".chat");
 
     listMessage.innerHTML = ``;
 
-    for (let i = 0; i < message.length; i++) {
-        if (message[i].type === "status") {
+    for (let i = 0; i < postedMessage.length; i++) {
+        if (postedMessage[i].type === "status") {
             listMessage.innerHTML += `
-            <li class="log"><p><span>(${message[i].time})</span><b> ${message[i].from}</b> ${message[i].text}<p></li>`;
-        } else if (message[i].type === "private_message" && (user === message[i].from || user === message[i].to)) {
+            <li class="log" id="message"><p><span>(${postedMessage[i].time})</span><b> ${postedMessage[i].from}</b> ${postedMessage[i].text}<p></li>`;
+        } else if (postedMessage[i].type === "private_message" && (user === postedMessage[i].from || user === postedMessage[i].to)) {
             listMessage.innerHTML += `
-            <li class="privately"><p><span>(${message[i].time})</span><b> ${message[i].from}</b> para<b> ${message[i].to}</b> ${message[i].text}<p></li>`;
+            <li class="privately id="message"><p><span>(${postedMessage[i].time})</span><b> ${postedMessage[i].from}</b> para<b> ${postedMessage[i].to}</b> ${postedMessage[i].text}<p></li>`;
         } else {
             listMessage.innerHTML += `
-            <li class="normal"><p><span>(${message[i].time})</span><b> ${message[i].from}</b> para<b> ${message[i].to}</b> ${message[i].text}<p></li>`;
+            <li class="normal" id="message"><p><span>(${postedMessage[i].time})</span><b> ${postedMessage[i].from}</b> para<b> ${postedMessage[i].to}</b> ${postedMessage[i].text}<p></li>`;
         }
     }
+
+    const messages = document.querySelector('.chat');
+    const lastMessage = messages.lastElementChild;
+    lastMessage.scrollIntoView();
+    console.log(lastMessage);
 
     setInterval(getMessage, 3000);
 }
 
 function statusLog() {
-    console.log(user);
+    /* console.log(user); */
     userStatus = { name: user };
-    console.log(userStatus);
+    /* console.log(userStatus); */
 
     const promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/status', userStatus);
-    promise.then(renderItWorked);
-    promise.catch(renderError);
+    promise.then(console.log("Log funcionando"));
+    promise.catch(console.log("Erro no log"));
 } 
+
+function sendMessage() {
+    message = document.querySelector('#submit').value;
+
+    postedMessage = { 
+                    from: user,
+                    to: "Todos",
+                    text: message,
+                    type: "message"
+                    };
+
+    const promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', postedMessage);
+    promise.then(console.log("mensagem enviada"));
+    promise.catch(console.log(window.location.reload()));
+    message = '';
+}
+
+document.addEventListener("keypress", function(enter) {
+
+    if(enter.key === "Enter") {
+        const sendButton = document.querySelector('#send');
+        sendButton.click();
+    }
+});
